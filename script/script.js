4c9a456e4fd2a6a401f775ec4edb31588f5ef0c6
penso996 <penso.andrea@outlook.it>
2025-01-17 17:34:25 +0100

    done card section on script

--- script/script.js
+++ script/script.js
@@ -39,3 +39,21 @@ const teamMembers = [
 
 // SELEZIONO GLI ELEMENTI NEL DOM
 const teamContainer = document.getElementById("team-container");
+
+// ITERO SU OGNI ELEMENTO DELL'ARRAY (MEMBRI DEL TEAM) E GENERO DELL'HTML
+teamMembers.forEach(function (member) {
+  const cardHTML = `
+  <div class="col-12 col-md-6 col-lg-4">
+    <div class="bg-black d-flex align-items-center">
+      <img src="${member.img}" style="width: 100px; height: 100px; object-fit: cover;" class="me-3">
+      <div>
+        <h5 class="text-white fw-bold mb-0">${member.name.toUpperCase()}</h5>
+        <p class="text-white mb-0">${member.role}</p>
+        <a href="mailto:${member.email}" class="text-decoration-none">${member.email}</a>
+      </div>
+    </div>
+  </div>
+`;
+
+  teamContainer.innerHTML += cardHTML;
+});
